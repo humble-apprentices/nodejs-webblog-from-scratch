490a53fc758816dce7ca1eaba13e4199ba7a9e15
黄锴 <huangkai18@meituan.com>
2022-06-21 16:00:53 +0800

    [project] Automatically setup data directory; provide data file access utilities for some extra ergonomics.

--- project/index.mjs
+++ project/index.mjs
@@ -1,13 +1,53 @@
-import { fileURLToPath } from 'node:url';
+import { constants } from 'node:fs';
 import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
-import { readFile, writeFile } from 'node:fs';
+import { open, access, mkdir } from 'node:fs/promises';
 
-const getDataFilePath = name => join(
+const { O_RDWR, O_CREAT } = constants;
+
+const DATA_DIR = join(
   dirname(fileURLToPath(import.meta.url)),
   'data',
+);
+
+const checkOrSetUpDataDir = async () => {
+  try {
+    await access(DATA_DIR);
+  } catch(error) {
+    if (error.errno === -2) {
+      await mkdir(DATA_DIR);
+    }
+  }
+};
+
+const getDataFilePath = (name) => join(
+  DATA_DIR,
   name,
 );
+
+// fs constants: https://nodejs.org/api/fs.html#fspromisesconstants
+const writeDataFileContent = async (name, content) => {
+  const file = await open(getDataFilePath(name), O_RDWR);
+  await file.write(Buffer.from(content));
+  await file.sync();
+  file.close();
+  return content;
+};
+
+// fs constants: https://nodejs.org/api/fs.html#fspromisesconstants
+const readDataFileContent = async (name, defaultContent = '') => {
+  const file = await open(getDataFilePath(name), O_RDWR | O_CREAT);
+  const fileContent = await file.readFile('utf8');
+
+  if (!fileContent.length && defaultContent) {
+    await file.write(Buffer.from(defaultContent));
+    await file.sync();
+  }
+
+  file.close();
+  return fileContent || defaultContent;
+};
 
 let title = null;
 const TITLE_FILE = getDataFilePath('title.txt');
@@ -48,11 +88,7 @@ const server = createServer((req, res) => {
         location: '/',
       }).end();
 
-      writeFile(TITLE_FILE, title, err => {
-        if (err) {
-          console.error(err);
-        }
-      });
+      writeDataFileContent('title.txt', title);
     });
 
     return;
@@ -62,7 +98,11 @@ const server = createServer((req, res) => {
   res.end('404');
 });
 
-readFile(TITLE_FILE, (err, content) => {
-  title = err ? '你好世界' : content.toString();
-  server.listen(3333);
-});
+const start = async (port) => {
+  await checkOrSetUpDataDir();
+  title = await readDataFileContent('title.txt', '你好世界');
+  server.listen(port);
+};
+
+start(3333);
+
